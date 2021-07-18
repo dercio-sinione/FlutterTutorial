4fb3309ec3c31f5771fa8447c9334a766d50bbad
dercio-sinione <derciosinione@gmail.com>
2021-07-19 00:20:38 +0100

    Button Section

---
+++ lib/textSection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextSection extends StatelessWidget {
+  const TextSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
