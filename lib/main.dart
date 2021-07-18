4c5a9e0e3cba5a2d440465e2d2100f1844a41ba1
dercio-sinione <derciosinione@gmail.com>
2021-07-18 22:27:11 +0100

    Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,62 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(TotorialHome());
+class Counter extends StatefulWidget {
+  // This class is the configuration for the state.
+  // It holds the values (in this case nothing) provided
+  // by the parent and used by the build  method of the
+  // State. Fields in a Widget subclass are always marked
+  // "final".
 
-class TotorialHome extends StatelessWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      // This call to setState tells the Flutter framework
+      // that something has changed in this State, which
+      // causes it to rerun the build method below so that
+      // the display can reflect the updated values. If you
+      // change _counter without calling setState(), then
+      // the build method won't be called again, and so
+      // nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Home',
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: null,
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation Menu',
-          ),
-          title: Text('Flutter Totorial'),
-          actions: [
-            IconButton(
-              onPressed: null,
-              icon: Icon(Icons.search),
-            )
-          ],
+    // This method is rerun every time setState is called,
+    // for instance, as done by the _increment method above.
+    // The Flutter framework has been optimized to make
+    // rerunning build methods fast, so that you can just
+    // rebuild anything that needs updating rather than
+    // having to individually changes instances of widgets.
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: _increment,
+          child: Text('Increment'),
         ),
-        body: Center(
-          child: Text('Hello World!!!'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          tooltip: 'Add',
-          child: Icon(Icons.add),
-        ),
-      ),
+        SizedBox(width: 16),
+        Text('Count: $_counter'),
+      ],
     );
   }
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Counter(),
+        ),
+      ),
+    ),
+  );
 }
