41cdd18361b5c8992454023af060ca33a6e4d83c
dercio-sinione <derciosinione@gmail.com>
2021-07-18 22:56:11 +0100

    Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Home',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Flutter Tutorial"),
+          ),
+          body: Column(
+            children: [
+              Center(
+                child: Text('Hello Flutter!!'),
+              )
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/titleSection.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class titleSection extends StatelessWidget {
+  const titleSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello Derone'),
+    );
+  }
+}
