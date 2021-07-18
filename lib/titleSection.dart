d4715bbb04a5bddf8194d20f98225696c39739ae
dercio-sinione <derciosinione@gmail.com>
2021-07-19 00:18:50 +0100

    Button Section

--- lib/titleSection.dart
+++ lib/titleSection.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class titleSection extends StatelessWidget {
-  const titleSection({Key? key}) : super(key: key);
+class TitleSection extends StatelessWidget {
+  const TitleSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
