3f98c3df34695cf7f0ecd8247364ac96a85ea42d
dercio-sinione <derciosinione@gmail.com>
2021-07-18 23:09:30 +0100

    First Component

--- lib/titleSection.dart
+++ lib/titleSection.dart
@@ -6,7 +6,35 @@ class titleSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Hello Derone'),
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                color: Colors.red[500],
+                child: Text(
+                  'Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              )
+            ],
+          )),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41')
+        ],
+      ),
     );
   }
 }
